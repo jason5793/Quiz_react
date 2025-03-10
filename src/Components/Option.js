eabf21942352837c501d0b20b134349e05f6ab8b
Jason Dhekial Pyrbot <jasonvlog81@gmail.com>
2025-03-10 16:15:01 +0530

    adding components for quiz projects

---
+++ src/Components/Option.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Option extends Component {
+  render() {
+    const{option,selectedOption,onOptionChange}=this.props;
+    return (    
+    <div className='options'>
+        {option.map((option, index) => (
+            <div key={index} className="form-check">
+                <input 
+                    type='radio'
+                    name='option'
+                    value={option}
+                    checked={selectedOption===option}
+                    onChange={onOptionChange}
+                    className='form-check-labal'/>
+                    <label className='form-check-label'>{option}</label>
+            </div>
+        ))}
+        </div>
+    )
+  }
+}
+export default Option;
